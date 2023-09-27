154dfb9b16032aacc7aaaecbc82b2f63ec0516e8
大石头 <nnhy@vip.qq.com>
2023-09-27 23:52:51 +0800

    Git代码库同步，保持本地代码库到多个远程库之间的同步

---
+++ GitSync/Program.cs
@@ -0,0 +1,18 @@
+﻿using GitSync;
+using Stardust;
+
+//!!! 轻量级控制台项目模板
+
+// 启用控制台日志，拦截所有异常
+XTrace.UseConsole();
+
+// 初始化对象容器，提供注入能力
+var services = ObjectContainer.Current;
+services.AddStardust();
+
+// 注册后台任务 IHostedService
+var host = services.BuildHost();
+host.Add<Worker>();
+
+// 异步阻塞，友好退出
+await host.RunAsync();
---
+++ GitSync/Worker.cs
@@ -0,0 +1,16 @@
+﻿using NewLife.Remoting;
+using NewLife.Serialization;
+using Stardust;
+
+namespace GitSync;
+
+/// <summary>
+/// 后台任务。支持构造函数注入服务
+/// </summary>
+public class Worker : BackgroundService
+{
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        return Task.CompletedTask;
+    }
+}
