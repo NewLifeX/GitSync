63d660431fd107bfa60070574765fff4ed8340e3
大石头 <nnhy@vip.qq.com>
2024-03-23 09:50:16 +0800

    考虑到执行时间到达时计算机可能未启动，下次启动时，如果错过了某次执行，则立马执行。

--- GitSync/MyService.cs
+++ GitSync/MyService.cs
@@ -22,6 +22,22 @@ internal class MyService : ServiceBase
         SyncSetting.Provider.Changed += (s, e) => CheckTimer();
 
         base.StartWork(reason);
+
+        // 考虑到执行时间到达时计算机可能未启动，下次启动时，如果错过了某次执行，则立马执行。
+        var set = SyncSetting.Current;
+        foreach (var tm in _timers)
+        {
+            if (tm.Cron != null && set.LastSync.Year > 2000)
+            {
+                var next = tm.Cron.GetNext(set.LastSync);
+                if (next < DateTime.Now)
+                {
+                    WriteLog("错过了[{0}]的执行时间{1}，立即执行", tm.Cron, set.LastSync);
+                    tm.SetNext(-1);
+                    break;
+                }
+            }
+        }
     }
 
     protected override void StopWork(String reason)
@@ -78,6 +94,10 @@ internal class MyService : ServiceBase
         var tracer = ServiceProvider.GetService<ITracer>();
         var worker = new Worker(null, tracer);
         await worker.ExecuteAsync(default);
+
+        var set = SyncSetting.Current;
+        set.LastSync = DateTime.Now;
+        set.Save();
 
         WriteLog("同步完成！");
 
--- GitSync/SyncSetting.cs
+++ GitSync/SyncSetting.cs
@@ -16,6 +16,10 @@ public class SyncSetting : Config<SyncSetting>
     [Description("Cron表达式。控制定时执行，多个分号隔开")]
     public String Crons { get; set; }
 
+    /// <summary>最后同步。记录最后一次同步时间。考虑到执行时间到达时计算机可能未启动，下次启动时，如果错过了某次执行，则立马执行。</summary>
+    [Description("最后同步。记录最后一次同步时间。考虑到执行时间到达时计算机可能未启动，下次启动时，如果错过了某次执行，则立马执行。")]
+    public DateTime LastSync { get; set; }
+
     /// <summary>集合</summary>
     [Description("集合")]
     public Repo[] Repos { get; set; }
